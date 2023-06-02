89b2851dc378f96efb472044a83995c28a468f74
Koiitoh <103555286+Koiitoh@users.noreply.github.com>
2023-06-01 22:24:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var  resetbutton = document.querySelector("button");
+var resetbutton = document.querySelector("button");
+
 resetbutton.addEventListener("click", function(event) {
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
+    var element = document.getElementsByTagName("div");
+    for (index = element.length - 1; index >= 0; index--) {
+        element[index].parentNode.removeChild(element[index]);
+    }
+    event.stopPropagation();
+});
 
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
+addEventListener("click", function(event) {
     var dot = document.createElement("div");
     dot.className = "dot";
     dot.style.left = (event.pageX - 4) + "px";
     dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
-  });
+});
